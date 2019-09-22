47fea02379672630c5f6bc470d0a7e989090c662
github username <github email address>
2019-09-22 14:34:02 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,38 +1,27 @@
 class School
-    # attr_accessor = :school_name, :roster, :add_student, :grade
-    
-
-    def initialize(school_name)
-        @school_name = school_name
-        @roster = {}
+    attr_accessor :name, :roster
+  
+    def initialize(name)
+      @name = name
+      @roster = {}
     end
-
-    def roster
-        @roster 
+  
+    def add_student(student_name, grade)
+      roster[grade] ||= []
+      roster[grade] << student_name
     end
-
-    def add_student(name, grade)
-        @student_name = name
-        @grade = grade
-        if @roster.include?(grade) == false
-          @roster[grade] = []
-        end
-        @roster[grade] << name
+  
+    def grade(student_grade)
+      roster[student_grade]
+    end
+  
+    # this method should arrange the students in each grade by alphabetical order
+    def sort
+      sorted = {}
+      roster.each do |grade, students|
+        sorted[grade] = students.sort
       end
-
-      def grade(grade)
-        @students << grade
-      end
-
-      def grade(number)
-        @roster[number]
-      end
-
-      def sort
-        @roster.each do |grade, name|
-            @roster[grade] = name.sort
-        end
-      end
-
-end
+      sorted
+    end
+  end
 
